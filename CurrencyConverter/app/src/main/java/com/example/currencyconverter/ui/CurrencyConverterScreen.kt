108a86620c4b62c34d902acf99331319696ef51f
VanChien411 <phuthienchien3@gmail.com>
2025-01-06 02:23:53 +0700

    Create dark and light themes. Update currency conversion functionality

--- CurrencyConverter/app/src/main/java/com/example/currencyconverter/ui/CurrencyConverterScreen.kt
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/ui/CurrencyConverterScreen.kt
@@ -1,12 +1,11 @@
 package com.example.currencyconverter.ui.screens
 
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -30,48 +29,35 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.currencyconverter.R
 import com.example.currencyconverter.viewmodel.CurrencyViewModel
+
 var viewModel: CurrencyViewModel = CurrencyViewModel()
 
 @Preview()
 @Composable
 fun CurrencyConverterScreen() {
 
-    val exchangeRates by viewModel.exchangeRates.observeAsState()
-    val moneyTypes by viewModel.moneyTypes.observeAsState()
-
+    viewModel.fetchExchangeRates()
     Column ( modifier = Modifier
         .fillMaxSize()
-        .background(Color.White).windowInsetsPadding(WindowInsets.statusBars),
+        .background( MaterialTheme.colorScheme.background).windowInsetsPadding(WindowInsets.statusBars),
         horizontalAlignment = Alignment.CenterHorizontally)
     {
-        viewModel.fetchExchangeRates()
-        if (exchangeRates == null) {
-            Text("Loading exchange rates...")
-        } else {
-            // Hiển thị tỷ giá
-            val rates = exchangeRates!!.rates // Lấy tỷ giá
-            Text("Base currency: ${exchangeRates!!.base}")
 
-            LazyColumn {
-                items(rates.entries.toList()) { rate ->
-                    Text("${rate.key}: ${rate.value}")
-                }
-            }
-        }
+//        Load()
         TextHeader("Chuyển đổi tiền tệ")
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.White)
-                .padding(16.dp),
-
+                .padding(16.dp)
+                ,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
 
 
             Spacer(modifier = Modifier.height(10.dp))
-            CutLeftBox(fromNumber = 0.0, toNumber = 0.0, from = "usd", to = "vnd", list = moneyTypes?.toList(), result = "sefsef" )
+            // Kiểm tra các giá trị nullable và tránh lỗi null bằng cách sử dụng safe call (?.) và elvis operator (?:)
+            CutLeftBox()
 
 
             //        // Khi chưa có dữ liệu
@@ -102,13 +88,20 @@ fun CurrencyConverterScreen() {
 
 }
 @Composable
+fun Load(){
+    val exchangeRates by viewModel.exchangeRates.observeAsState()
+    val moneyTypes by viewModel.moneyTypes.observeAsState()
+
+}
+
+@Composable
 fun TextHeader(
     content: String,
-    color: Color = Color.White,
-    style: TextStyle = MaterialTheme.typography.headlineMedium, // Kiểu chữ mặc định
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    style: TextStyle = MaterialTheme.typography.headlineSmall, // Kiểu chữ mặc định
     fontWeight: FontWeight? = FontWeight.Bold, // Trọng lượng chữ tùy chỉnh
     textAlign: TextAlign = TextAlign.Center, // Căn chỉnh văn bản
-    modifier: Modifier = Modifier.background(Color.Black).padding(0.dp,10.dp).fillMaxWidth() // Modifier mặc định
+    modifier: Modifier = Modifier.padding(0.dp,10.dp).fillMaxWidth() // Modifier mặc định
 
 ) {
     Text(
@@ -127,7 +120,7 @@ fun NumberMoney(
     number: Double,
     color: Color = Color.Black,
     style: TextStyle = MaterialTheme.typography.bodyMedium,
-    onValueChange: () -> Unit = {}
+    onChageText: (Double) -> Unit = {}
 ) {
     val formattedNumber = String.format("%.2f", number) // Định dạng số với 2 chữ số thập phân
 //    Text(
@@ -140,21 +133,20 @@ fun NumberMoney(
 //            .height(70.dp)
 //            .wrapContentHeight(Alignment.CenterVertically) // Căn giữa nội dung theo chiều dọc
 //    )
-    var text by remember { mutableStateOf(formattedNumber) }
 
-    TextField(
-        value = text,
+
+    OutlinedTextField(
+        value = number.toString(),
         onValueChange = { newValue ->
             // Kiểm tra nếu giá trị nhập vào là một số hợp lệ
             if (newValue.isEmpty() || newValue.toDoubleOrNull() != null) {
-                text = newValue
-                onValueChange()
+                newValue.toDoubleOrNull()?.let { onChageText(it) }
             }
         },
         keyboardOptions = KeyboardOptions.Default.copy(
             keyboardType = KeyboardType.Number // Chỉ hiển thị bàn phím số
         ),
-        textStyle = TextStyle(color = color, fontWeight = FontWeight.Bold,fontSize = 25.sp,), // Màu văn bản của TextField
+        textStyle = TextStyle(color = color, fontWeight = FontWeight.Bold,fontSize = 20.sp,), // Màu văn bản của TextField
         colors = TextFieldDefaults.colors(
             //setting the text field background when it is focused
             focusedContainerColor = Color.White,
@@ -169,15 +161,16 @@ fun NumberMoney(
         ),
 
         modifier = Modifier
-            .height(70.dp)
+            .height(55.dp)
             .wrapContentHeight(Alignment.CenterVertically) // Căn giữa nội dung theo chiều dọc
     )
 }
 
 @Composable
-fun TextLabel(content: String,fontSize: TextUnit = 20.sp, textAlign: TextAlign = TextAlign.Left,color: Color= Color.Black) {
-    Text(
-        text = content,
+fun TextLabel(content: String?,fontSize: TextUnit = 17.sp, textAlign: TextAlign = TextAlign.Left,color: Color= Color.Black) {
+    content?.let {
+        Text(
+        text = it,
         style = MaterialTheme.typography.headlineMedium, // Kiểu chữ tiêu đề
         fontWeight = FontWeight.Bold,
         color =color,
@@ -186,17 +179,18 @@ fun TextLabel(content: String,fontSize: TextUnit = 20.sp, textAlign: TextAlign =
             .fillMaxWidth(),
         textAlign = textAlign,
     )
+    }
 
 }
 @Composable
-fun TextWithDropdown( money:Double,onChageText: (Double)-> Unit = {},  style: TextStyle = MaterialTheme.typography.bodyLarge, moneyType : String? = null, items : List<String>? = null) {
+fun TextWithDropdown( money:Double,onChageText: (Double)-> Unit = {}, onChageRate:(String)-> Unit ={},  style: TextStyle = MaterialTheme.typography.bodyLarge, moneyType : String? = null, items : List<String>? = null) {
     var expanded by remember { mutableStateOf(false) }
-    var selectedMoneyType  by remember { mutableStateOf(moneyType) }
+
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .height(70.dp)
+            .height(55.dp)
             .fillMaxWidth()
             .clip(RoundedCornerShape(8.dp)) // Giới hạn nền bên trong đường viền
             .background(Color.White) // Áp dụng nền trước
@@ -209,9 +203,9 @@ fun TextWithDropdown( money:Double,onChageText: (Double)-> Unit = {},  style: Te
     ) {
         Column(
 
-            modifier = Modifier.padding(15.dp,0.dp).weight(1f) // Take up the remaining space
+            modifier = Modifier.padding(1.dp,0.dp).weight(1f) // Take up the remaining space
         ) {
-            NumberMoney(number = money)
+            NumberMoney(number = money, onChageText = onChageText)
         }
         Column(
             horizontalAlignment = Alignment.End,
@@ -220,19 +214,20 @@ fun TextWithDropdown( money:Double,onChageText: (Double)-> Unit = {},  style: Te
 
         ) {
             Row( verticalAlignment = Alignment.CenterVertically,) {
-                selectedMoneyType?.let {
+
                     Text(
-                        text = it,
-                        fontSize = 25.sp,
+                        text = moneyType?:"", // Hiển thị trạng thái local
+                        fontSize = 20.sp,
                         style = style,
                         fontWeight = FontWeight.Bold,
+                        color = Color.Black,
                         textAlign = TextAlign.Right,
                         modifier = Modifier
                             .fillMaxHeight()
                             .wrapContentHeight(Alignment.CenterVertically)
 
                     )
-                }
+
                 IconButton(onClick = {  expanded = !expanded}) {
 
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "More options",Modifier.width(30.dp))
@@ -244,13 +239,16 @@ fun TextWithDropdown( money:Double,onChageText: (Double)-> Unit = {},  style: Te
                     items?.forEach { item ->
                         DropdownMenuItem(
                             text = { Text(item) },
-                            onClick = { selectedMoneyType = item
+                            onClick = {
+                                onChageRate(item)
+                   
                                 expanded = !expanded}
                         )
 
                     }
 
                 }
+
             }
 
         }
@@ -258,8 +256,10 @@ fun TextWithDropdown( money:Double,onChageText: (Double)-> Unit = {},  style: Te
 
     }
 }
+
 @Composable
 fun ButtonDefault(onClick: () -> Unit, text: String) {
+
     Button(
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(
@@ -267,19 +267,29 @@ fun ButtonDefault(onClick: () -> Unit, text: String) {
             contentColor = Color.Black         // Màu chữ đen
         ),
         shape = RoundedCornerShape(4.dp),
-        contentPadding = PaddingValues(20.dp),
+        contentPadding = PaddingValues(17.dp),
         modifier = Modifier.padding(5.dp) // Thêm padding xung quanh nếu cần
     ) {
         Text(text = text,
             fontSize = 22.sp,
             style = MaterialTheme.typography.labelLarge)
 
+
     }
 }
 @Composable
-fun CutLeftBox(fromNumber: Double ,toNumber:Double,list: List<String>?, from: String, to:String, result: String) {
+fun CutLeftBox() {
+
+    val list by viewModel.moneyTypes.observeAsState()
+    val result by viewModel.result.observeAsState()
+    val fromNumber by viewModel.fromNumber.observeAsState()
+    val toNumber by viewModel.toNumber.observeAsState()
+    val fromRate by viewModel.fromRate.observeAsState(initial = "")
+    val toRate by viewModel.toRate.observeAsState(initial = "")
+    val timeLine by viewModel.timeLine.observeAsState(initial = "")
+
     Surface(
-        shape = AbsoluteCutCornerShape(topRightPercent = 10),
+        shape = AbsoluteCutCornerShape(topRightPercent = 5),
         modifier = Modifier.fillMaxWidth()
     ) {
         Box(
@@ -305,21 +315,26 @@ fun CutLeftBox(fromNumber: Double ,toNumber:Double,list: List<String>?, from: St
                 ) {
                     Column {
                         TextLabel("Số tiền")
-                        TextWithDropdown(fromNumber, moneyType = from , items = list)
-                        Spacer(modifier = Modifier.height(20.dp))
+                        TextWithDropdown(fromNumber?:0.0, moneyType = fromRate , items = list?.toList() ?: emptyList(), onChageText ={ newValue -> viewModel.updateFromNumber(newValue)}, onChageRate = {newValue -> viewModel.updateFromRate(newValue)})
+                        Spacer(modifier = Modifier.height(15.dp))
                         Image(
                             painter = painterResource(id = R.drawable.swap_calls), // Thay R.drawable.image bằng tên tệp ảnh của bạn
                             contentDescription = "Image Description", // Mô tả cho ảnh
-                            modifier = Modifier.fillMaxWidth().height(40.dp) // Thêm modifiers nếu cần
+                            modifier = Modifier.fillMaxWidth().height(30.dp)
+                                .clickable {
+                                    // Sự kiện khi nhấn vào ảnh
+                                   viewModel.rateSwap()
+                                }
                         )
                         TextLabel("Chuyển thành")
-                        TextWithDropdown(toNumber, moneyType = to, items = list)
+                        TextWithDropdown(toNumber?:0.0, moneyType = toRate, items = list?.toList() ?: emptyList(), onChageText ={ newValue -> viewModel.updateToNumber(newValue)}, onChageRate = {newValue -> viewModel.updateToRate(newValue)})
                     }
 
                 }
-                TextLabel(result, fontSize = 25.sp, textAlign = TextAlign.Center)
+
+                TextLabel(result, fontSize = 20.sp, textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.onBackground)
                 Row (
-                    modifier = Modifier .padding(vertical = 10.dp) // Khoảng cách phía trên và dưới
+                    modifier = Modifier .padding(vertical = 5.dp) // Khoảng cách phía trên và dưới
                 ){
                     Box(
                         modifier = Modifier
@@ -331,8 +346,8 @@ fun CutLeftBox(fromNumber: Double ,toNumber:Double,list: List<String>?, from: St
                 }
 
                 Row(){
-                    ButtonDefault(onClick = { }, "Chuyển đổi")
-                    TextLabel("Thời gian cập nhập giá\n12/32/3333 hh/mm/ss", fontSize = 15.sp, textAlign = TextAlign.Right, color = Color.Gray)
+                    ButtonDefault(onClick = { viewModel.exchangeRates()}, "Chuyển đổi")
+                    TextLabel("Thời gian cập nhập giá\n$timeLine", fontSize = 15.sp, textAlign = TextAlign.Right, color = Color.Gray)
 
                 }
 
--- CurrencyConverter/app/src/main/java/com/example/currencyconverter/viewmodel/CurrencyViewModel.kt
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/viewmodel/CurrencyViewModel.kt
@@ -1,9 +1,7 @@
 package com.example.currencyconverter.viewmodel
 
+import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,6 +9,10 @@ import androidx.lifecycle.viewModelScope
 import com.example.currencyconverter.data.network.ExchangeRatesDataResponse
 import com.example.currencyconverter.data.repository.CurrencyRepository
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class CurrencyViewModel() : ViewModel() {
     private val repository: CurrencyRepository = CurrencyRepository()
@@ -18,24 +20,32 @@ class CurrencyViewModel() : ViewModel() {
     val exchangeRates: LiveData<ExchangeRatesDataResponse> = _exchangeRates
     private val _moneyTypes = MutableLiveData<Set<String>>()
     val moneyTypes: LiveData<Set<String>> get() = _moneyTypes
+    private val _result = MutableLiveData<String>("")
+    val result: LiveData<String> get() = _result
 
-    private val _fromRate = MutableLiveData<String>()
+    private val _fromNumber = MutableLiveData<Double>(0.0)
+    val fromNumber: LiveData<Double> get() = _fromNumber
+    private val _toNumber = MutableLiveData<Double>(0.0)
+    val toNumber: LiveData<Double> get() = _toNumber
+
+    private val _fromRate = MutableLiveData<String>("")
     val fromRate: LiveData<String> get() = _fromRate
-    private val _toRate = MutableLiveData<String>()
+    private val _toRate = MutableLiveData<String>("")
     val toRate: LiveData<String> get() = _toRate
+    private val _timeLine = MutableLiveData<String>("")
+    val timeLine: LiveData<String> get() = _timeLine
 
-    var fromNumber by mutableStateOf(0.0)
-        private set
-
-    var toNumber by mutableStateOf(0.0)
-        private set
-
-    fun updateFromNumber(value: Double) {
-        fromNumber = value
+    fun updateToNumber(newValue: Double) {
+        _toNumber.value = newValue
     }
-
-    fun updateToNumber(value: Double) {
-        toNumber = value
+    fun updateFromNumber(newValue: Double) {
+        _fromNumber.value = newValue
+    }
+    fun updateFromRate(newValue: String){
+        _fromRate.value = newValue
+    }
+    fun updateToRate(newValue: String){
+        _toRate.value = newValue
     }
 
 
@@ -46,35 +56,61 @@ class CurrencyViewModel() : ViewModel() {
                 _exchangeRates.postValue(it)
 
                 _moneyTypes.postValue(it.rates.keys)
+                // Kiểm tra số lượng phần tử trong rates
+                when (it.rates.size) {
+                    1 -> {
+                        // Nếu có 1 phần tử, gán cho cả _fromRate và _toRate
+                        val firstKey = it.rates.keys.first()
+                        _fromRate.postValue(firstKey)
+                        _toRate.postValue(firstKey)
+                    }
+                    2 -> {
+                        // Nếu có 2 phần tử, lấy 2 key đầu tiên và gán cho _fromRate và _toRate
+                        val ratesList = it.rates.keys.take(2).toList()  // Lấy 2 phần tử đầu tiên của map rates
+                        val a = ratesList[0]  // Key đầu tiên
+                        val b = ratesList[1]  // Key thứ hai
 
+                        _fromRate.postValue(a)  // Lấy key đầu tiên và gán cho _fromRate
+                        _toRate.postValue(b)    // Lấy key thứ hai và gán cho _toRate
+                    }
+                    else -> {
+                        // Nếu có nhiều hơn 2 phần tử, chỉ lấy 2 phần tử đầu tiên và gán cho _fromRate và _toRate
+                        val ratesList = it.rates.keys.take(2).toList()  // Lấy 2 phần tử đầu tiên của map rates
+                        val a = ratesList[0]  // Key đầu tiên
+                        val b = ratesList[1]  // Key thứ hai
+
+                        _fromRate.postValue(a)  // Lấy key đầu tiên và gán cho _fromRate
+                        _toRate.postValue(b)    // Lấy key thứ hai và gán cho _toRate
+                    }
+                }
             } ?: run {
                 Log.e("ExchangeRates", "Failed to fetch exchange rates")
             }
+
         }
     }
-    fun exchangeRates(fromNumber: Double = 0.0 , from:String, to: String  ){
+
+    @SuppressLint("DefaultLocale")
+    fun exchangeRates(){
         viewModelScope.launch {
-            val rates = repository.getExchangeRatesData()
-            rates?.let {
-                _exchangeRates.postValue(it)
 
-                _moneyTypes.postValue(it.rates.keys)
+            val rates =_exchangeRates.value
 
-            } ?: run {
-                Log.e("ExchangeRates", "Failed to fetch exchange rates")
-            }
 
 // Lấy tỷ giá từ đồng tiền cơ sở (USD) sang `fromCurrency` và `toCurrency`
-            val fromRate = rates?.rates?.get(from)
-            val toRate = rates?.rates?.get(to)
+            val fromRate = rates?.rates?.get(_fromRate.value)
+            val toRate = rates?.rates?.get(_toRate.value)
+
             // Kiểm tra nếu một trong hai tỷ giá không tồn tại
             if (fromRate == null || toRate == null) {
                 Log.e("ExchangeRates", "Failed to fetch exchange rates")
             }
             else{
-                updateFromNumber(fromNumber)
-                updateToNumber(( fromNumber / fromRate ) * toRate)
 
+                _toNumber.value = _fromNumber.value?.div(fromRate)?.times(toRate)
+                val newToNumber = String.format("%.3f", _toNumber.value ?: 0.0)
+                _result.postValue(_fromNumber.value.toString()+ " " + _fromRate.value +" = "  + newToNumber +" "+_toRate.value)
+                _timeLine.value = convertTimestampToDateTime(rates.timestamp)
             }
 
 
@@ -82,15 +118,21 @@ class CurrencyViewModel() : ViewModel() {
     }
 
     fun rateSwap(){
-        if (_toRate.value != null || _fromRate.value == null) {
+        if (_toRate.value == null || _fromRate.value == null) {
             Log.e("ExchangeRates", "Failed to fetch exchange rates")
             return
         }
-
-            exchangeRates(fromNumber = fromNumber, from = _toRate.value!!, to = _fromRate.value!!)
+            val temp = _fromRate.value
+            _fromRate.value = _toRate.value
+            _toRate.value = temp.toString()
+            exchangeRates()
 
 
     }
-
+    fun convertTimestampToDateTime(timestamp: Long): String {
+        val date = Date(timestamp * 1000) // Nhân với 1000 để chuyển giây thành mili giây
+        val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()) // Định dạng ngày giờ
+        return format.format(date) // Chuyển thành chuỗi định dạng
+    }
 
 }
